65798f42e9eb8bb04b9d4c32e9976fd01b35924c
Leohne <bricemagneville.pro@gmail.com>
2024-03-01 16:02:52 +0100

    Build MTF

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    assetsInclude: ['../src/assets/**','../src/assets/img/projet/**','../src/assets/img/icone/**' ],
-  },
+  plugins: [react()]
 })
